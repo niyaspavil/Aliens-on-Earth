d4bfe64814e0812d4ae3bc8fcfdef5db69f89b72
niyaspavil <pavilniyas@gmail.com>
2014-10-28 17:05:15 +0530

    tested Alien object sucessfully

---
+++ Aliens_on_Earth/test/test_admin.py
@@ -0,0 +1,7 @@
+from .. import admin
+
+def test_Aliens():
+    details = ('test_name',' test_color','1','2','test_planet')
+    alien = admin.Aliens(details)
+    assert alien.code_name == 'test_name'
+    assert alien.home_planet == 'test_planet'
