3d9c660c4665cf9baf4eeb90922fadd9dc337c5a
niyaspavil <pavilniyas@gmail.com>
2014-10-28 09:27:39 +0530

    created registration form

---
+++ register.py
@@ -0,0 +1,15 @@
+
+def register():
+    print "Welcome to Registration."
+    code_name = prompt('Enter your name code:')
+    blood_color = prompt('Enter your blood color:')
+    no_of_antennas = prompt('Enter your number of antennas:')
+    no_of_legs = prompt('Enter your number of legs:')
+    home_planet = prompt('Enter your Home Planet:')
+    
+
+def prompt(message):
+   return raw_input(message)
+
+    
+        
